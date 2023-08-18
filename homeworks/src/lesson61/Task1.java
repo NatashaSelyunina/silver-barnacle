53708ad2a63793de587d7229769c898758997b79
Natalya Selyunina <seluninanv@mail.ru>
2023-08-18 07:41:51 +0300

    feat: sequence size and reading a number

--- homeworks/src/lesson61/Task1.java
+++ homeworks/src/lesson61/Task1.java
@@ -1,10 +1,19 @@
 package lesson61;
 
+import java.util.Scanner;
+
 public class Task1 {
 
   // Найти минимальное четное число в последовательности положительных чисел
   // или вывести -1, если такого числа не существует
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    int size = scanner.nextInt();
+    for (int i = 0; i < size; i++) {
+      int x = scanner.nextInt();
+      if (x < 0) {
+        return;
+      }
+    }
   }
 }
