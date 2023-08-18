7863a18a71a0467f8233055d860a223e9b55a28d
Natalya Selyunina <seluninanv@mail.ru>
2023-08-18 07:59:40 +0300

    feat: ArrayList numbers

--- homeworks/src/lesson61/Task1.java
+++ homeworks/src/lesson61/Task1.java
@@ -1,5 +1,7 @@
 package lesson61;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Task1 {
@@ -9,18 +11,16 @@ public class Task1 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int size = scanner.nextInt();
-    int x = scanner.nextInt();
-    if (x <= 0) {
-      return;
-    }
-    int min = x;
-    for (int i = 1; i < size; i++) {
-      x = scanner.nextInt();
+
+    List<Integer> numbers = new ArrayList<>();
+
+    for (int i = 0; i < size; i++) {
+      int x = scanner.nextInt();
       if (x <= 0) {
         return;
       }
-      if (x < min) {
-        min = x;
+      if (x % 2 == 0) {
+        numbers.add(x);
       }
     }
   }
