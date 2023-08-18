1baed389648da4d2cb8f70df756dd365dd425dee
Natalya Selyunina <seluninanv@mail.ru>
2023-08-18 08:15:45 +0300

    feat: search min element in arrayList

--- homeworks/src/lesson61/Task1.java
+++ homeworks/src/lesson61/Task1.java
@@ -9,19 +9,33 @@ public class Task1 {
   // Найти минимальное четное число в последовательности положительных чисел
   // или вывести -1, если такого числа не существует
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int size = scanner.nextInt();
+    Scanner scanner = new Scanner(System.in);                    // O(1)
+    int size = scanner.nextInt();                                // O(1)
 
-    List<Integer> numbers = new ArrayList<>();
+    List<Integer> numbers = new ArrayList<>();                   // O(1)
 
     for (int i = 0; i < size; i++) {
       int x = scanner.nextInt();
       if (x <= 0) {
+        System.out.println("Число должно быть больше 0!");
         return;
       }
       if (x % 2 == 0) {
         numbers.add(x);
       }
     }
+
+    if (numbers.size() == 0) {
+      System.out.println(-1);
+      return;
+    }
+
+    int min = numbers.get(0);
+    for (int i = 1; i < numbers.size(); i++) {
+      if (numbers.get(i) < min) {
+        min = numbers.get(i);
+      }
+    }
+    System.out.println(min);
   }
 }
