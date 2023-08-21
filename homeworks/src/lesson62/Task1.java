0f61cdbbc8763d8e0f0b17c9267d7fa4252c1fcc
Natalya Selyunina <seluninanv@mail.ru>
2023-08-21 07:57:53 +0300

    feat: Task1 condition

---
+++ homeworks/src/lesson62/Task1.java
@@ -0,0 +1,11 @@
+package lesson62;
+
+public class Task1 {
+
+  // Напишите рекурсивный метод public static String reverse(int x),
+  // который будет возвращать строку, содержащую десятичные цифры числа
+  // в обратном порядке (развернёт число).
+  public static void main(String[] args) {
+
+  }
+}
