0edce90951d4e6aa59b72ef4e91f3ba803b3c4f2
Natalya Selyunina <seluninanv@mail.ru>
2023-08-21 23:49:55 +0300

    feat : fields and constructor Book.java

--- homeworks/src/lesson63/Book.java
+++ homeworks/src/lesson63/Book.java
@@ -1,10 +1,19 @@
 package lesson63;
 
-public class Task1 {
+public class Book {
 
   // Используйте класс Book из домашнего задания к уроку 53
   // но без интерфейса Comparable.
   // Создайте список книг и отсортируйте книги по автору,
   // (при одинаковых авторах - по названиям)
   // при помощи анонимного класса в качестве компаратора.
+  private final String author;
+  private final String title;
+  private final int pages;
+
+  public Book(String author, String title, int pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+  }
 }
