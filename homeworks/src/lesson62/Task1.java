dc5561a0791cf8cfb8f13c596d0ca4a7cb68be9b
Natalya Selyunina <seluninanv@mail.ru>
2023-08-21 08:06:33 +0300

    feat: String reverse(int x)

--- homeworks/src/lesson62/Task1.java
+++ homeworks/src/lesson62/Task1.java
@@ -1,11 +1,21 @@
 package lesson62;
 
+import java.util.Scanner;
+
 public class Task1 {
 
   // Напишите рекурсивный метод public static String reverse(int x),
   // который будет возвращать строку, содержащую десятичные цифры числа
   // в обратном порядке (развернёт число).
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+  }
 
+  public static String reverse(int x) {
+    if (x < 10) {
+      return Integer.toString(x);
+    }
+    return Integer.toString(Integer.parseInt((x % 10) + reverse(x / 10)));
   }
 }
+
