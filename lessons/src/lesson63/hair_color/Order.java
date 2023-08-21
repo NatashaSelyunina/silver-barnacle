a2e7535ee4f983f984068694df602b2c08ee26cc
Natalya Selyunina <seluninanv@mail.ru>
2023-08-21 21:15:34 +0300

    feat : add Order.calculatePrice

--- lessons/src/lesson63/hair_color/Order.java
+++ lessons/src/lesson63/hair_color/Order.java
@@ -1,9 +1,12 @@
 package lesson63.hair_color;
 
 import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Order {
 
+  private static final double PRICE_PER_CM = 1.5; // евро за сантиметр длины
+  private static final double MULTIPLIER_PER_COLOR = 0.2; // доля за каждый цвет
   private String name;         // имя заказчика
   private LocalTime startTime; // время начала
   private int colors;          // количество цветов
@@ -15,6 +18,7 @@ public class Order {
     this.startTime = startTime;
     this.colors = colors;
     this.length = length;
+    calculatePrice();
   }
 
   public String getName() {
@@ -55,5 +59,33 @@ public class Order {
 
   public void setPrice(double price) {
     this.price = price;
+  }
+
+  /**
+   * обновление поля <code>price</code> с использованием значений <code>length</code> и
+   * <code>colors</code>
+   */
+  private void calculatePrice() {
+    price = PRICE_PER_CM * length + MULTIPLIER_PER_COLOR * colors;
+  }
+
+  /**
+   * Интерактивное чтение данных о заказе
+   *
+   * @param scanner источник данных
+   * @return прочитанный заказ
+   */
+  public static Order interactiveRead(Scanner scanner) {
+    System.out.print("Введите имя заказчика: ");
+    String name = scanner.nextLine();
+    System.out.print("Введите время начала: ");
+    LocalTime startTime = LocalTime.parse(scanner.nextLine());
+    System.out.print("Укажите количество цветов: ");
+    int colors = scanner.nextInt();
+    scanner.nextLine();
+    System.out.print("Укажите длину волос в сантиметрах: ");
+    double length = scanner.nextDouble();
+    scanner.nextLine();
+    return new Order(name, startTime, colors, length);
   }
 }
