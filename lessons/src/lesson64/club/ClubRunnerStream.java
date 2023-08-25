65aec3b9a070342a79a6fbd36b07166399980706
Natalya Selyunina <seluninanv@mail.ru>
2023-08-25 06:45:00 +0300

    feat : class ClubRunnerStream

--- lessons/src/lesson64/club/ClubRunnerStream.java
+++ lessons/src/lesson64/club/ClubRunnerStream.java
@@ -1,5 +1,20 @@
 package lesson64.club;
 
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class ClubRunnerStream {
 
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Club club = new Club("Ромашка", 5, 18);
+    club.greet();
+
+    Stream.generate(() -> Vizitor.interactiveRead(scanner))
+        .filter(vizitor -> vizitor.getAge() >= club.getAllowedAge())
+        .limit(club.getCapacity())
+        .sorted((v1, v2) -> Integer.compare(v1.getAge(), v2.getAge()))
+        .forEach(vizitor -> System.out.println(vizitor));
+    club.close();
+  }
 }
